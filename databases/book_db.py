b7aaa3f00d90e5a4b8f37f8eb1c67933439824c9
Aharon <0583265262r@gmail.com>
2026-06-14 17:46:52 +0300

    handel update

--- databases/book_db.py
+++ databases/book_db.py
@@ -1,9 +1,9 @@
-from databases.db_connection import get_connection
+from databases.db_connection import *
 
-
+connection1 = DBConnection()
 class BookDB:
     def __init__(self):
-        self.conn = get_connection
+        self.conn = connection1.get_connection()
     
     def create_book(self,data:dict)->None:
         conn =self.conn()
@@ -44,11 +44,26 @@ class BookDB:
             raise KeyError
         return row
 
-    def update_book(self,id):
+    def update_book(self,id:int,data:dict):
         conn =self.conn()
         cursor = conn.cursor(dictionary=True)
+        set_parts = []
+        for key in data.keys():
+            set_parts.append(f"{key}=%s")
+        set_keys = ", ".join(set_parts)
+        query = f"UPDATE books SET {set_keys} WHERE id = %s"
+        values = list(data.values()) + [id]
+        cursor.execute(query,values)
+        conn.commit()
+        changed = cursor.rowcount > 0
+        conn.close()
+        cursor.close()
+        if not changed:
+            raise ValueError
+        return changed
+
+
         
-        pass
     def set_available(self,id,val,member_id):
         pass
     def count_total_books(self):
@@ -61,3 +76,11 @@ class BookDB:
         pass
     def count_active_borrows_by_member(self,member_id):
         pass
+
+if __name__ == "__main__":
+    c1 = BookDB()
+    c1.update_book(1,{
+  "genre": "Science",
+  "available_is": 0,
+  "borrowed_by_member_id": 1
+})
--- routes/book_routes.py
+++ routes/book_routes.py
@@ -15,7 +15,7 @@ def create_book(data : CreateBook):
         add_book = bookdb.create_book({"title":data.title,"author":data.author,"genre":data.genre})
         return f"add book {add_book} was success "
     except: 
-        raise HTTPException(status_code=404,detail="genre not found")
+        raise HTTPException(status_code=404)
 
 
 
@@ -31,11 +31,20 @@ def get_book_by_id(id:int):
     except:
         raise HTTPException(status_code=404,detail="id not found")
 class UpdateBook(BaseModel):
-        title: str 
-        author: str 
-        genre: str
-@router.put("books/{id}")
-def update_book_by_id(id,data:CreateBook):
+        title: str |None = None
+        author: str |None = None
+        genre: str |None = None
+        available_is: bool |None = None
+        borrowed_by_member_id: int |None = None
+
+@router.put("/books/{id}")
+def update_book_by_id(id:int,data:UpdateBook):
+    try:
+        update = bookdb.update_book(id,data)
+        return update
+    except:
+        raise HTTPException(status_code=404)
+
 
 if __name__ == "__main__":
     uvicorn.run(router="book_routes:router",reload=True)
